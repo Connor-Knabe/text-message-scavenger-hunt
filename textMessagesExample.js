7cd5f141ee7b1a03fd3743dfe94328d3ff4cbb80
Connor Knabe <connorknabe@gmail.com>
2019-07-25 07:57:34 -0500

    adding intro

--- textMessagesExample.js
+++ textMessagesExample.js
@@ -1,1 +1,3 @@
-module.exports.textContent = { 15: ['this is your first clue', 'second clue'], 16: ['this is your first clue', 'second clue'] };
+module.exports.clues = { 15: ['this is your first clue', 'second clue'], 16: ['this is your first clue', 'second clue'] };
+module.exports.intro = { 15: ['this is your first clue', 'second clue'], 16: ['this is your first clue', 'second clue'] };
+module.exports.greetings = { 15: ['this is your first clue', 'second clue'], 16: ['this is your first clue', 'second clue'] };
